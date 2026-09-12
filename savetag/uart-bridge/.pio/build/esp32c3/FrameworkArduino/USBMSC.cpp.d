b638790b004e122e5dd50f70aed723990266f7d9
qvp-tech <qvp.tech@gmail.com>
2026-09-13 00:32:34 +0700

    chore: them .gitignore, loai bo file build

--- savetag/uart-bridge/.pio/build/esp32c3/FrameworkArduino/USBMSC.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32c3/FrameworkArduino/USBMSC.cpp.o: \
- C:/Users/Admin/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.cpp \
- C:/Users/Admin/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
- C:/Users/Admin/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32c3/qio_qspi/include/sdkconfig.h
